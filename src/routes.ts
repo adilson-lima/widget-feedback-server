8a617b291e0689184f079f377c8595c9aae4bf3c
Adilson Lima <adilson.pereira.lima@gmail.com>
2022-05-06 19:06:56 -0300

    init

---
+++ src/routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+import { prisma } from "./prisma";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "59c8d28e70da12",
+    pass: "a670cf52b8e852",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Adilson Pereira Lima <adilson.pereira.lima@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment} </p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  res.status(201).json({ data: feedback });
+});
